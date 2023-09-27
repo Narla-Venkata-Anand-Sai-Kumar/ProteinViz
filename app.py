df264f1833eb5f3fcd3e6a447a4b8e80cbbe92d1
Anand <venkatnarla0@gmail.com>
2023-09-27 10:32:17 +0530

    new Update

--- app.py
+++ app.py
@@ -4,15 +4,58 @@ from rdkit.Chem import AllChem
 import require as py3Dmol
 
 app = Flask(__name__)
-
+def codon_category(codon):
+     codon_dict={"UUU":"Phe","UUC":"Phe","UUA":"Leu","UUA":"Leu",
+                 "UCU":"Ser","UCC":"Ser","UCA":"Ser","UCG":"Ser",
+                 "UAU":"Tyr","UAC":"Tyr","UAA":"Stop","UAG":"Stop",
+                 "UGU":"Cys","UGC":"Cys","UGA":"Stop","UGG":"Trp",
+                 "CUU":"Leu","CUC":"Leu","CUA":"Leu","CUG":"Leu",
+                 "CCU":"Pro","CAC":"Pro","CCA":"Pro","CCG":"Pro",
+                 "CAU":"His","CAC":"His","CAA":"Gln","CAG":"Gln",
+                 "CGU":"Arg","CGC":"Arg","CGA":"Arg","CGG":"Arg",
+                 "AUU":"Ile","AUC":"Ile","AUA":"Ile","AUG":"Met",
+                 "ACU":"Thr","ACC":"Thr","ACA":"Thr","ACG":"Thr",
+                 "AAU":"Asn","AAC":"Asn","AAA":"Lys","AAG":"Lys",
+                 "AGU":"Ser","AGC":"Ser","AGA":"Arg","AGG":"Arg",
+                 "GUU":"Val","GUC":"Val","GUA":"Val","GUG":"Val",
+                 "GCU":"Ala","GCC":"Ala","GCA":"Ala","GCA":"Ala",
+                 "GAU":"Asp","GAC":"Asp","GAA":"Glu","GAG":"Glu",
+                 "GGU":"Gly","GGC":"Gly","GGA":"Gly","GGG":"Gly"}
+     return codon_dict[codon]
+def amino_acid(sequence):
+     sequence_length=len(sequence)-3
+     amino=""
+     for i in range(0,sequence_length,3):
+         code=str(sequence[i]+sequence[i+1]+sequence[i+2])
+         amino+=codon_category(code)
+         amino+='-'
+     return amino[:-1]
+def Smile_String(smile):
+     Smile_dict={"Ala":"CC(C(=O)O)N","Cys":" CSCC(C(=O)O)N","Asp":"CC(C(=O)O)C(=O)O",
+                 "Glu":"CC(C(=O)O)C(=O)OC(C)C","Phe":"Nc1ccccc1C(=O)O",
+                 "Gly":"NCC(=O)O","His":"Nc1c[nH]cn1C(=O)O","Ile":"CCC(C)C(C(=O)O)N",
+                 "Lys":"NCCCCC@HN","Leu":"CCC(C)CC(C(=O)O)N","Met":"CSCCC(C(=O)O)N",
+                 "Asn":"CC(C(=O)O)C(=O)N","Pro":"CC1CC(C(=O)N)C1","Gln":"CC(C(=O)O)C(=O)NC",
+                 "Arg":"N=C(N)NCCCCC@HN","Ser":"OCC(C(=O)O)N","Thr":"CC(C(=O)O)C(O)N",
+                 "Val":"CCC(C)C(C(=O)O)N","Trp":"Nc1ccc2c(c1)CC@@HN2","Tyr":"Nc1ccc(O)cc1C(=O)O"}
+     return Smile_dict[smile]
+ 
 # Define the route for the homepage
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         smiles = request.form['smiles']
+        dump_smile = smiles
+        k=amino_acid(smiles)
+        S=k.split('-')
+        smiles = ""
+        for i in S:
+            smiles+=Smile_String(i)
+        print(smiles)
         mol = Chem.MolFromSmiles(smiles)
 
         if mol is not None:
+            
             AllChem.EmbedMolecule(mol, randomSeed=42)
 
             viewer = py3Dmol.view(width=500, height=500)
@@ -57,7 +100,7 @@ def index():
          <h2>Computational Structural Biology</h2>
         
         </div>
-        <h1 style="font-size: 36px;">3D visualization of {smiles}</h1>
+        <h1 style="font-size: 36px;">3D visualization of {dump_smile}</h1>
     </section>
     <section id="3dview" style="display: flex; justify-content: center; align-items: center; height: 60vh;">
         {html_main}
